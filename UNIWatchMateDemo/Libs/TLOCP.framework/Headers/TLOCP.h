69f86a41e801ed0f8240814dac312b1fdbd184ec
qiaychao <qiyachao@shenjugroup.com>
2025-02-08 14:22:59 +0800

    主项目增加TLOCP库

---
+++ UNIWatchMateDemo/Libs/TLOCP.framework/Headers/TLOCP.h
@@ -0,0 +1,18 @@
+//
+//  TLOCP.h
+//  TLOCP
+//
+//  Created by t_t on 2025/2/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TLOCP.
+FOUNDATION_EXPORT double TLOCPVersionNumber;
+
+//! Project version string for TLOCP.
+FOUNDATION_EXPORT const unsigned char TLOCPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TLOCP/PublicHeader.h>
+
+
